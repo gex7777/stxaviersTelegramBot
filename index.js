0f6f4840bbd8c3993ea976accc0b0b566b1dc5cf
George <gex7777@gmail.com>
2023-04-10 12:22:39 +0530

    add env
    done

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = "5813939145:AAFVaZCZTUVk2YEaEwwWeaBZai7amUnCUIY";
+const token = process.env.TELEGRAM_BOT_API;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
